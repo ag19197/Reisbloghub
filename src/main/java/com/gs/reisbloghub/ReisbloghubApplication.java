4019c7314f72a62199c42f7526deb2cdc15d05e9
ag1919 <1229550645@qq.com>
2026-03-05 21:36:50 +0800

    框架及依赖

---
+++ src/main/java/com/gs/reisbloghub/ReisbloghubApplication.java
@@ -0,0 +1,13 @@
+package com.gs.reisbloghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReisbloghubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReisbloghubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gs/reisbloghub/ReisbloghubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gs.reisbloghub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReisbloghubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
